02639a40f23b005982a13404b7c2faa52150650f
Kuldoshev Jasurbek <113510212+221199cs18@users.noreply.github.com>
2023-01-31 15:28:27 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -10,10 +10,25 @@
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    string name = "Jasur", surname = "Kuldoshev", groupName = "22CS23";
-    int studenId = 221199;
-    cout << name << " " << surname << "\n";
-    cout << groupName << "\n";
-    cout << studenId << "\n";
+    /* Problem 1*/
+//    cout << "Subject        ";
+//    cout << "Marks" << endl;
+//    cout << "--------------------" << endl;
+//    cout << "Mathematics    ";
+//    cout << "90" << endl;
+//    cout << "Computer       ";
+//    cout << "77"     << endl;
+//    cout << "Chemistr       ";
+//    cout << "69" << endl;
+    /* Problem 2*/
+//    int a = 0, b = 0;
+//    cout << "Enter first number: ";cin >> a;
+//    cout << "Enter second number: ";cin >> b;
+//    cout << "The sum is: " << a + b << endl;
+    /* Problem 3*/
+//    char ch;
+//    cout << "Enter any characters: ";cin >> ch;
+//    cout << "ASCII equivalent is: " << int(ch) << endl;
+    
     return 0;
 }
