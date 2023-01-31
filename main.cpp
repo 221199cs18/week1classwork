f0b6d14b09fa47e8c91139c72405a90e54a75ff3
Kuldoshev Jasurbek <113510212+221199cs18@users.noreply.github.com>
2023-01-31 15:04:00 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  homework1
+//
+//  Created by Jasurbek Kuldoshev on 31/01/23.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    string name = "Jasur", surname = "Kuldoshev", groupName = "22CS23";
+    int studenId = 221199;
+    cout << name << " " << surname << "\n";
+    cout << groupName << "\n";
+    cout << studenId << "\n";
+    return 0;
+}
